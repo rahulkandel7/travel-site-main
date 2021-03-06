ab3c2cb1bb4c128bcb724111007cffa5cec991ea
rahul sharma <rahulsharma@rahuls-MacBook-Air.local>
2021-03-06 08:12:27 +0545

    complete homepage

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -15,31 +15,97 @@
             <a href="#">
                 <img src="{{ asset('img/demo.jpg') }}" alt="" class="img-fluid">
             </a>
+            <div class="item__overlay">
+                <h2 class="item__overlay-heading">
+                    Heading Here
+                </h2>
+                <p class="item__overlay--description">
+                    image description here
+                </p>
+                <a href="#" class="item__overlay--btn">
+                    Know more
+                </a>
+            </div>
         </div>
         <div class="item">
             <a href="">
                 <img src="{{ asset('img/romance.jpg') }}" alt=""  class="img-fluid">
             </a>
+            <div class="item__overlay">
+                <h2 class="item__overlay-heading">
+                    Heading Here
+                </h2>
+                <p class="item__overlay--description">
+                    image description here
+                </p>
+                <a href="#" class="item__overlay--btn">
+                    Know more
+                </a>
+            </div>
         </div>
         <div class="item">
             <a href="">
                 <img src="{{ asset('img/passport.jpg') }}" alt=""  class="img-fluid">
             </a>
+            <div class="item__overlay">
+                <h2 class="item__overlay-heading">
+                    Heading Here
+                </h2>
+                <p class="item__overlay--description">
+                    image description here
+                </p>
+                <a href="#" class="item__overlay--btn">
+                    Know more
+                </a>
+            </div>
         </div>
         <div class="item">
             <a href="">
                 <img src="{{ asset('img/flight.jpg') }}" alt=""  class="img-fluid">
             </a>
+            <div class="item__overlay">
+                <h2 class="item__overlay-heading">
+                    Heading Here
+                </h2>
+                <p class="item__overlay--description">
+                    image description here
+                </p>
+                <a href="#" class="item__overlay--btn">
+                    Know more
+                </a>
+            </div>
         </div>
         <div class="item">
             <a href="">
                 <img src="{{ asset('img/holy.jpg') }}" alt=""  class="img-fluid">
             </a>
+            <div class="item__overlay">
+                <h2 class="item__overlay-heading">
+                    Heading Here
+                </h2>
+                <p class="item__overlay--description">
+                    image description here
+                </p>
+                <a href="#" class="item__overlay--btn">
+                    Know more
+                </a>
+            </div>
         </div>
         <div class="item">
             <a href="">
                 <img src="{{ asset('img/romance.jpg') }}" alt=""  class="img-fluid">    
             </a>
+            <div class="item__overlay">
+                <h2 class="item__overlay-heading">
+                    Heading Here
+                </h2>
+                <p class="item__overlay--description">
+                    image description here
+                </p>
+                <a href="#" class="item__overlay--btn">
+                    Know more
+                </a>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -7,16 +7,16 @@
         <center><hr class="card-line marb hidden slideinUp"></center>
         <div class="row">
             <div class="col-md-3 col-6 ico">
-                <div class="circle">
+                <div class="circle mrl-5">
                     <i class="fas fa-user-shield service-icon"></i>
                 </div>
-                <p class="service-text">Assured Best Fares</p>
+                <p class="service-text mrl-4">Assured Best Fares</p>
             </div>
             <div class="col-md-3 col-6 ico">
-                <div class="circle">
+                <div class="circle mrl-5">
                     <i class="fas fa-headset service-icon"></i>
                 </div>
-                <p class="service-text">24*7 Support</p>
+                <p class="service-text mrl-5">24*7 Support</p>
             </div>
             <div class="col-md-3 col-6 ico">
                 <div class="circle">

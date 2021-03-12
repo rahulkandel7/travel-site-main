14e698959dd320cbee7723527333a05b532cd676
rahul sharma <rahulsharma@rahuls-MacBook-Air.local>
2021-03-12 12:58:59 +0545

    added insurance page

---
+++ resources/views/insurance/inside.blade.php
@@ -0,0 +1,73 @@
+<div class="ourservice-text hidden slideinUp">
+    <p class="our">Pangolin <span class="servicess">Insurance Service</span></p>
+</div>
+<div class="container-fluid">
+    <div class="row shadow">
+        <div class="col-md-12" style="border-right:1px solid rgba(201, 196, 196, 0.52); ">
+            <p class="forex_text" style="padding-top:1rem">
+            Nothing beats the joy of knowing that all your travel needs can be 
+            fulfilled under one roof. We are licensed by the Reserve Bank of India 
+            to provide you with the best currency exchange rates. Equipped with 
+            extensive knowledge and years of experience, our Forex Team mainly 
+            focuses on safeguarding your funds.
+            </p>
+            <p class="forex_text">
+            You can also opt for Traveller’s Cheques or Prepaid Travel Cards offered 
+            by us. These are safer and more convenient options while travelling 
+            abroad. Moreover, we provide safe and secure international money 
+            transfers.
+            </p>
+            <p class="forex_text">
+            In order to help us understand your Forex requirements a little 
+            better, please drop in your query and our representative will get 
+            back to you.
+            </p>
+            <p class="forex_text">
+            You can also visit the nearest Riya Branch or write to us at forex.
+            support@riya.travel with your queries.
+            </p>
+
+            <h2 class="forex_from-heading">
+            Have a Question? Submit an inquiry and we will get back to you.
+            </h2>
+            <div class="container">
+            <form action="#">
+                <div class="row">
+                    <div class="input-field col s12">
+                    <input id="text" type="text" required>
+                    <label for="text"> * Name</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                    <input id="email" type="email" required>
+                    <label for="email"> * Email</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                    <input id="number" type="number" required>
+                    <label for="number"> * Phone Number</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                    <input id="city" type="text" required>
+                    <label for="city"> * City</label>
+                    </div>
+                </div>
+                <div class="row" style="margin-bottom:0;">
+                    <div class="input-field col s12">
+                    <textarea id="textarea1" class="materialize-textarea"></textarea>
+                    <label for="textarea1"> Tell us a bit more about your query</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                <input type="submit" value="Submit" class="forex_btn">
+                </div>
+            </form>
+            </div>
+        </div>
+        
+    </div>
+</div>
